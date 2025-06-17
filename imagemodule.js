220e671835a3f8641d3775b582e4d01b750ab369
mohamedelbas19 <mohamed.elbasri@idom.com>
2025-06-17 10:10:18 +0200

    Update imagemodule.js

--- imagemodule.js
+++ imagemodule.js
@@ -1,3 +1,4 @@
+//modified
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.ImageModule = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 "use strict";
 
@@ -127,7 +128,7 @@ module.exports = function () {
 			this.addExtensionRels("image/" + extension, extension);
 			var relationships = this.relsDoc.getElementsByTagName("Relationships")[0];
 			var newTag = this.relsDoc.createElement("Relationship");
-			newTag.namespaceURI = null;
+			//newTag.namespaceURI = null;
 			var maxRid = this.loadImageRels() + 1;
 			newTag.setAttribute("Id", "rId" + maxRid);
 			newTag.setAttribute("Type", "http://schemas.openxmlformats.org/officeDocument/2006/relationships/image");
@@ -4342,7 +4343,7 @@ Node.prototype = {
 	childNodes : null,
 	ownerDocument : null,
 	nodeValue : null,
-	namespaceURI : null,
+	//namespaceURI : null,
 	prefix : null,
 	localName : null,
 	// Modified in DOM Level 2:
